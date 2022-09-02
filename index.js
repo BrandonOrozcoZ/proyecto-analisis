abb05962e750fb24faeef20d7ac8a8478f7bab76
Brandon Orozco Z <brandons.orozcoz@uqvirtual.edu.co>
2022-09-01 19:53:11 -0500

    Estructura del proyecto

---
+++ index.js
@@ -0,0 +1,63 @@
+let QUESTION_INDEX = 0;
+let score = 0;
+
+loadQuestion(QUESTION_INDEX);
+
+function loadQuestion(index){
+    question = questionBase[index]
+
+    options = [...question.distractors];
+    options.push(question.answer);
+
+    options.sort(() => Math.random() - 0.5);
+
+    document.getElementById("question").innerHTML = question.question;
+    
+    if(question.image){
+        document.getElementById("image").src = question.image;
+        document.getElementById("image").style.display = "";
+    }else{
+        document.getElementById("image").style.display = "none";
+    }
+    
+
+    document.getElementById("1st-option").innerHTML = options[0];
+    document.getElementById("2nd-option").innerHTML = options[1];
+    document.getElementById("3rd-option").innerHTML = options[2];
+    document.getElementById("4th-option").innerHTML = options[3];
+}
+
+async function selectOption(index){
+    let flag = options[index] == question.answer
+
+    if(flag){
+        await Swal.fire({
+            title:"Respuesta correcta",
+            text:"La respuesta es correcta",
+            icon:"success",
+        });
+        score++;
+    }else{
+        await Swal.fire({
+            title:"Respuesta incorrecta",
+            text: `La respuesta correcta es "${question.answer}"`,
+            icon:"error",
+        });
+    }
+
+    QUESTION_INDEX++;
+    if(QUESTION_INDEX >= questionBase.length){
+        
+        QUESTION_INDEX = 0;
+        
+        await Swal.fire({
+            title: "Quiz finalizado",
+            text: `Tu puntaje es: ${score}/${questionBase.length}`,
+            icon: "success",
+        });
+        score = 0;
+    }
+    loadQuestion(QUESTION_INDEX);
+    
+    
+}
---
+++ question-base.js
@@ -0,0 +1,17 @@
+let questionBase = [
+    {
+        question: "¿Cuántas manzanas tiene juanito?",
+        image: "https://i.ibb.co/ZWb17qF/3370133965-66da51e38b-o.jpg",
+        answer: "4 Manzanas",
+
+        distractors:["3 Manzanas", "2 Manzanas", "1 Manzana"],
+
+    },
+    {
+        question: "¿Cuántas veces repetimos avanzada?",
+        image: "https://i.ibb.co/k9JLBqk/eeed769146b2bd51f7719c31af633799-c5-720x720.jpg",
+        answer: "2 veces",
+
+        distractors:["Sapo", "Antorchita", "Rojo"],
+    },
+];
